3057fce0ac0f9aa40aad37b6211693147449c7bc
ROCIO <ROCIO@HERNANDEZ>
2021-04-30 00:01:45 -0500

    Commit 2

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author ROCIO
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero1;
+        numero1 = 3;
+        int numero2;
+        numero2 = 7;
+        if(numero1 > numero2)
+        {System.out.println("El número" + numero1 + "es mayor y el numero menor es" + numero2);
+        }else{System.out.println("El número " + numero2 + " es mayor y el numero menor es" + numero1);
+        }
+    }
+    
+}
